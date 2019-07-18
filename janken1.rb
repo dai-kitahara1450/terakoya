6879e6419f3f20e1e2f341c85c6379f8e3eb7694
dai.kitahara <dai.kitahara@daikitaharanoMacBook-Pro.local>
2019-07-18 16:42:18 +0900

    janken1.rb new

---
+++ janken1.rb
@@ -0,0 +1,26 @@
+puts "最初はグー。じゃんけん"
+
+def janken
+  puts "数字を選べ
+  グー:0
+  チョキ:1
+  パー:2"
+
+  my_hand = gets.to_i
+  computer_hand = rand(0..2)
+
+  jankens = ["グー", "チョキ","パー"]
+  puts "私の手は#{jankens[my_hand]}, あなたの手:#{jankens[computer_hand]}"
+
+  if my_hand == computer_hand
+    puts "あいこでしょ"
+    return true
+  elsif(my_hand == 0 && computer_hand == 1)||(my_hand == 1 && computer_hand == 2)||(my_hand == 2 && computer_hand == 0)
+    puts "You win!"
+    return false
+
+  else
+    puts "You looooose"
+    return false
+  end
+end
